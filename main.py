dd390daaccc330c0ba00d93c9fdc32e3991529b6
ilinasf <155798764+ilinasf@users.noreply.github.com>
2024-03-30 11:46:17 +0500

    моя игра

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import pygame as pg
 
+pg.init()
+
 class Game:
     def __init__(self) -> object:
-        pass
+        window_size: tuple = (300,300)
+        pg.display.set_caption("Моя игра моя игра она же как и я")
+        self.screen = pg.display.set_mode(window_size)
+        backgroud_color = (0, 0, 255)   # синий
+
+        # заполняем фон заданным цветом
+        self.screen.fill(backgroud_color)
+
+
+        pg.display.flip()
+
+        while True:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
+                    exit()
+
+a = Game()
