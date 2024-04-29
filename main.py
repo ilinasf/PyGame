296db12c2f3ad1da311b46e5bece94392e73a97a
ilinasf <155798764+ilinasf@users.noreply.github.com>
2024-04-29 11:05:46 +0500

    kjkj

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
-import pygame as pg
+import pygame
 
-pg.init()
+class Window:
+    def __init__(self):
+        self.width = 600
+        self.height = 400
+        self.fps = 60
 
-class Game:
-    def __init__(self) -> object:
-        window_size: tuple = (300,300)
-        pg.display.set_caption("Моя игра моя игра она же как и я")
-        self.screen = pg.display.set_mode(window_size)
-        backgroud_color = (0, 0, 255)   # синий
+        self.__init_window()
 
-        # заполняем фон заданным цветом
-        self.screen.fill(backgroud_color)
+    def __init_window(self):
+        pygame.init()
+        pygame.mixer.init()
 
+    
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption('Моя игра')
+        clock = pygame.time.Clock()
 
-        pg.display.flip()
+class EventHandler:
+    def __init__(self):
+        while self.running:
+            clock.tick(self.fps)
+            for event in pygame.event.get():
+                if event.type ==  pygame.QUIT:
+                    running = False
+                    pygame.quit()
 
+class Run(Window, EventHandler):
+    def __init__(self):
+        super().__init__()
+        self.running = True
         while True:
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    pg.quit()
-                    exit()
+            self.screen.fill((0,0,255))
+            pygame.display.flip()
 
-a = Game()
+class Render:
+    def __init__(self):
+        pass
+
+k = Run()
